25424a717527a79bfb1764803243a31c53ae8371
Putra <com.owl.minerva@gmail.com>
2019-11-01 10:49:40 +0700

    push agogo

--- app/src/main/java/id/co/agogo/LoginActivity.kt
+++ app/src/main/java/id/co/agogo/LoginActivity.kt
@@ -1,18 +1,29 @@
 package id.co.agogo
 
+import android.Manifest
+import android.annotation.TargetApi
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.TextView
 import id.co.agogo.dialog.BottomPopUpNavigationMenu
 import android.content.pm.PackageManager
+import android.hardware.biometrics.BiometricPrompt
+import android.os.Build
+import android.os.CancellationSignal
 import android.view.WindowManager
 import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import java.util.*
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 import kotlin.concurrent.schedule
 
 class LoginActivity : AppCompatActivity(), BottomPopUpNavigationMenu.EditTextChangeListener {
     private lateinit var progressBar: ProgressBar
+    private lateinit var biometricPrompt: BiometricPrompt
     private lateinit var version: TextView
     private lateinit var username: EditText
     private lateinit var password: EditText
@@ -48,13 +59,69 @@ class LoginActivity : AppCompatActivity(), BottomPopUpNavigationMenu.EditTextCha
         }
     }
 
+    @TargetApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+            || ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+            || ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+            || ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+            || ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.FOREGROUND_SERVICE
+            ) != PackageManager.PERMISSION_GRANTED
+            || ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_PHONE_STATE
+            ) != PackageManager.PERMISSION_GRANTED
+            || ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.USE_BIOMETRIC
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(
+                arrayOf(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.FOREGROUND_SERVICE,
+                    Manifest.permission.READ_PHONE_STATE,
+                    Manifest.permission.USE_BIOMETRIC
+                ), 100
+            )
+        }
+
+        val executor = Executors.newSingleThreadExecutor()
+
         setContentView(R.layout.activity_login)
         progressBar = findViewById(R.id.loading_bar)
         version = findViewById(R.id.version)
         username = findViewById(R.id.username)
         password = findViewById(R.id.password)
+
+//        biometricPrompt = BiometricPrompt.Builder(this).setTitle("Finger Print")
+//            .setSubtitle("plise place your finger").setDescription("this is finger print")
+//            .setNegativeButton(
+//                "Cancel",
+//                executor,
+//                DialogInterface.OnClickListener { dialog, which ->
+//
+//                }).build()
+
         progressBar.progress = 100
         versionName = try {
             val pInfo = this.packageManager.getPackageInfo(packageName, 0)
@@ -80,8 +147,27 @@ class LoginActivity : AppCompatActivity(), BottomPopUpNavigationMenu.EditTextCha
         }
 
         password.setOnClickListener {
-            val popUpDialog = BottomPopUpNavigationMenu()
-            popUpDialog.show(supportFragmentManager, "Password TRX")
+            //            biometricPrompt.authenticate(CancellationSignal(), executor, object : BiometricPrompt.AuthenticationCallback() {
+//                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult?) {
+//                    super.onAuthenticationSucceeded(result)
+//                    Timer().schedule(500) {
+//                        runOnUiThread {
+//                            Toast.makeText(applicationContext, "awwwwwwwwwwwwwww", Toast.LENGTH_LONG).show()
+//                        }
+//                    }
+//                }
+//
+//                override fun onAuthenticationFailed() {
+//                    super.onAuthenticationFailed()
+//                    Timer().schedule(500) {
+//                        runOnUiThread {
+//                            Toast.makeText(applicationContext, "xxxxxxxxxxxxxxxxxxxxxx", Toast.LENGTH_LONG).show()
+//                        }
+//                    }
+//                }
+//            })
+//            val popUpDialog = BottomPopUpNavigationMenu()
+//            popUpDialog.show(supportFragmentManager, "Password TRX")
         }
     }
 }
--- app/src/main/java/id/co/agogo/dialog/BottomPopUpNavigationMenu.kt
+++ app/src/main/java/id/co/agogo/dialog/BottomPopUpNavigationMenu.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import id.co.agogo.R
+import java.util.*
+import kotlin.concurrent.schedule
 
 class BottomPopUpNavigationMenu : BottomSheetDialogFragment() {
     private var editTextChangeListener: EditTextChangeListener? = null
+    private lateinit var imm: InputMethodManager
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,10 +22,18 @@ class BottomPopUpNavigationMenu : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.botttom_pop_up_navigation_menu, container, false)
-
+        imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         val passwordTRX = view.findViewById<EditText>(R.id.password_trx)
+        Timer().schedule(500) {
+            passwordTRX.requestFocus()
+            imm.showSoftInput(passwordTRX, InputMethodManager.SHOW_IMPLICIT)
+        }
         passwordTRX.setOnClickListener {
             try {
+                Timer().schedule(500) {
+                    passwordTRX.clearFocus()
+                    imm.hideSoftInputFromWindow(passwordTRX.windowToken, 0)
+                }
                 editTextChangeListener?.onEditTextChange(passwordTRX.text.toString())
                 dismiss()
             } catch (e: java.lang.ClassCastException) {
